5f7477742e697c98cfa0d0596c7c12b0291616e8
jsantos-adilson <adilson.santos.eteot@gmail.com>
2022-09-19 21:27:57 -0300

    Mudando ordem div

--- script.js
+++ script.js
@@ -45,9 +45,6 @@ function showInfo(json){
 
     showWarning('');
 
-    // Altera o valor de 'none' para 'block'
-    document.querySelector('.resultado').style.display = 'block';
-
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
@@ -55,6 +52,9 @@ function showInfo(json){
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
 
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+
+    // Altera o valor de 'none' para 'block'
+    document.querySelector('.resultado').style.display = 'block';
 }
 
 function showWarning(msg){
